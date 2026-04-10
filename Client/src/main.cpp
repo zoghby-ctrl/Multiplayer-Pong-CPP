e8f35d8ecb7b1ad0dd0054b2cb907d11ebf0ca84
Zoghby <692501203@ecu.edu.eg>
2026-04-10 21:41:12 +0200

    Fixing
    Fix Common project structure and include paths

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "protocol.h"
 
 int main()
 {
--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "protocol.h"
 int main()
 {
     std::cout << "Server scaffold ready.\n";
