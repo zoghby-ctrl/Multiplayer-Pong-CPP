6cfdceee559828bfdffdd8a73714848505caddb6
Zoghby <692501203@ecu.edu.eg>
2026-04-10 05:35:18 +0200

    Add protocol header with default port and packet types

--- Common/include/protocol.h
+++ Common/include/protocol.h
@@ -1,1 +1,13 @@
+#pragma once
 
+namespace Common
+{
+    constexpr int DefaultPort = 7777;
+
+    enum class PacketType
+    {
+        Unknown = 0,
+        Input,
+        StateSnapshot
+    };
+}
