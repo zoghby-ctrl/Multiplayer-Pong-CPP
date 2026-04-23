bef76d4d15c9a2b7298fca3d9664c53ee07ad6be
NOUR ELDIN AMR <collagenour1@gmail.com>
2026-04-24 01:04:12 +0300

    I MADE THE GAME LOOP
    I MADE THE GAME LOOP FROM WAITING TO WINNIG AND GAMEOVER

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,45 +1,101 @@
 #include <iostream>
 #include <Windows.h>   // for GetAsyncKeyState
 #include "../../Common/include/protocol.h"
-
-
 using namespace std;
+enum class MatchState {
+    WAITING,
+    IN_PROGRESS,
+    GAME_OVER,
+    RESET
+};
+
+
 int main()
 {
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    MatchState state = MatchState::WAITING; // always initialize 
+
+    // MAKES THE GAME ALWAYS IN STANDBY MODE 
+    //state = MatchState::GAME_OVER; syntax is differ from the the OG class
+    
+    const int WIN_SCORE = 5;// the score from 5
+    ///////////////////////////////////////////////////////////////////////////////////////////
   cout << "Client running...\n";
     bool gameRunning = true;
     int moveDirection = 0; // the player idle state or no movement
+    ///////////////////////////////////////////////////////////////////////////////////////////
     Protocol::PlayerInput input;
-
+    Protocol::GameState SCORE_OF_GAME;
+    ///////////////////////////////////////////////////////////////////////////////////////////
     DWORD lastSendTime = GetTickCount();//gives current time in milliseconds --> THE GEtTICKCOUNT (premade)
     const DWORD sendInterval = 50; // البرنامج بيقرأ الزرار فورًا بدون تأخير 
     // THATS THE DELAY TO THE SERVER  
 
     while (gameRunning)
     {
-        input.up = false;
-        input.down = false;
-        // so to refresh the keyboard input , good for best performance
-        if (input.up = (GetAsyncKeyState('W') & 0x8000) != 0 || (GetAsyncKeyState(VK_UP) & 0x8000)) {
+        if (state == MatchState::IN_PROGRESS) {
+            input.up = false;
+            input.down = false;
+            // so to refresh the keyboard input , good for best performance
+            if (input.up = (GetAsyncKeyState('W') & 0x8000) != 0 || (GetAsyncKeyState(VK_UP) & 0x8000)) {
 
-           input.up = true;
+                input.up = true;
 
+            }
+            /* if pressed w we have to add hexadecimal  called holding behavior there is another code as & 0x0001
+            and if we didnt add it , it may act weird
+            */
+            else if (input.down = (GetAsyncKeyState('S') & 0x8000) != 0 || (GetAsyncKeyState(VK_DOWN) & 0x8000)) {
+
+                input.down = true;
+
+
+            }
+            else if ((GetAsyncKeyState(VK_ESCAPE) & 0x8000) != 0) {
+
+                gameRunning = false;
+            }
+
+            // read keyboard & send input
+            
         }
-        /* if pressed w we have to add hexadecimal  called holding behavior there is another code as & 0x0001
-        and if we didnt add it , it may act weird 
-        */
-        else if(input.down = (GetAsyncKeyState('S') & 0x8000) != 0 ||( GetAsyncKeyState(VK_DOWN) & 0x8000)) {
-
-            input.down = true;
-
-
+        else if (state == MatchState::WAITING) {
+            cout << "Waiting for opponent...\n";
         }
-        else if ((GetAsyncKeyState(VK_ESCAPE) & 0x8000) != 0) {
+        else if (state == MatchState::GAME_OVER) {
+            cout << "Game Over! Score: " << SCORE_OF_GAME.leftScore << "\n";
+            cout << "Game Over! Score: " << SCORE_OF_GAME.rightScore << "\n";
+            cout << "Press R to play again or ESC to quit\n";
 
-            gameRunning = false;
-            //
+            if ((GetAsyncKeyState('R') & 0x8000) != 0)
+            {
+                // tell server this player wants a rematch
 
+                state = MatchState::RESET;
+                // GO TO LINE (state == MatchState::RESET)
+            }
+            else if ((GetAsyncKeyState(VK_ESCAPE) & 0x8000) != 0)
+            {
+                gameRunning = false;
+            }
+            // wait for RESET signal from server
         }
+     
+        else if (state == MatchState::RESET)
+        {
+            // clear scores and positions locally
+            SCORE_OF_GAME.leftScore = 0;
+            SCORE_OF_GAME.rightScore = 0;
+            moveDirection = 0;
+            input.up = false;
+            input.down = false;
+            // مفيش حركه فوق او تحت كله ثابت حتى بدايه القيم الجاي
+
+            cout << "Restarting...\n";
+
+            state = MatchState::WAITING;
+        }
+      
 
         // direction 
 
@@ -72,6 +128,7 @@ int main()
         cout << "Up: " << input.up << " Down: " << input.down << endl;
         cout << "moving direction "<< moveDirection;
         Sleep(16); // slow down output (just for testing)
+    }
       
         // 1) read keyboard تم
         // 2) update paddle movement immediately تم
@@ -81,7 +138,7 @@ int main()
         // 
         //player 1 uses W/S
         //player 2 uses ↑ / ↓
-    }
+   
 
 
     return 0;
